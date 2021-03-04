cfe5a146ebb115901f9747921fc78d25c07246cb
Alojży <alojzy231@gmail.com>
2021-03-04 16:22:20 +0100

    Favicon

--- app.py
+++ app.py
@@ -61,8 +61,9 @@ def update(id):
         return render_template('update.html', task=task)
 
 @app.route('/favicon.ico') 
-def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static', 'favicon'), 'favicon.png', mimetype='image/png')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
